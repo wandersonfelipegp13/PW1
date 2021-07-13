ffa27240890bde8b648ce195e463f415b49538c4
Wanderson <wandersonfelipeplus13@gmail.com>
2021-07-13 19:08:00 -0300

    embaralha agrora gera 2 posiçoes repetidas no loop

--- src/m5/e1/tarefa/js/jogo.js
+++ src/m5/e1/tarefa/js/jogo.js
@@ -2,10 +2,6 @@
  var acerto = [ 0, 0, 0, 0, 0, 0 ];
  var viradas = 0;
  
- var pontos = 0;
- var carta1 = 0;
- var carta2 = 0;
-
 embaralha();
 
 function embaralha() {
@@ -14,19 +10,23 @@ function embaralha() {
     for (i = 0; i < cartas.length; i++) {
         var loop = true;
         var ok = false;
+        var repitidos = 0 ;
         var num;
         while (loop) {
-            num = Math.floor(Math.random() * 6 + 1);
-            for (var j = 0; j < cartas.length; j++) {
-                if (cartas[j] == num) {
-                    ok = false;
-                    break;
+            num = Math.floor(Math.random() * 3 + 1);
+            for (let value of cartas) {
+                if (value == num) {
+                    repitidos++;
+                    if (repitidos > 1) {
+                        ok = false;
+                        break;
+                    }
                 } else {
                     ok = true;
                 }
             }
             if (ok) {
-                loop = false;
+                // loop = false;
                 break;
             }
         }
@@ -39,33 +39,19 @@ function vira(carta) {
     
     viradas++;
     
-    if (carta1 == 0) {
-        carta1 = carta;
-    } else {
-        carta2 = carta;
+    var c = document.getElementsByTagName("img");
+    
+    for (var z = 0; z < cartas.length; z++) {
+        c[z].src = "img/star.png";
     }
     
-    if (carta1 == carta2) {
-        pontos++;
-        acerto[carta] = 1;
-        acerto[carta] = 1;
-    }
-    
-    if (carta == 0) {
-        document.getElementById("carta1").src = "img/" + cartas[carta] + ".png";
-    } else if (carta == 1) {
-        document.getElementById("carta2").src = "img/" + cartas[carta] + ".png";
+    if (carta == 1) {
+        document.getElementById("carta1").src = "img/star.png";
     } else if (carta == 2) {
-        document.getElementById("carta3").src = "img/" + cartas[carta] + ".png";
-    } else if (carta == 3) {
-        document.getElementById("carta4").src = "img/" + cartas[carta] + ".png";
-    } else if (carta == 4) {
-        document.getElementById("carta5").src = "img/" + cartas[carta] + ".png";
-    } else if (carta == 5) {
-        document.getElementById("carta6").src = "img/" + cartas[carta] + ".png";
+        document.getElementById("carta1").src = "img/ray.png";
+    } else {
+       document.getElementById("carta1").src = "img/core.png";
     }
-    
-    pontuacao();
   
 }
 
